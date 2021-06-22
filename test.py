71a38464891c324c2c50e12fdb50f7126dc96a2a
Andy M <navercm519@gmail.com>
2021-06-21 21:52:15 -0400

    main function ready for testing

--- test.py
+++ test.py
@@ -1,9 +1,58 @@
 import urllib.request
 
+InFile = "cities.txt"
+OutFile = "out.txt"
 
-zvHttpTrip = r'https://www.travelmath.com/driving-time/from/Lowell,+MA/to/Holbrook,+MA'
-        
-zvTripOut = urllib.request.urlopen(zvHttpTrip).read()
-zvTripOutFmt = zvTripOut.decode()
+# ---------------------------------------------------------------------------------------------------
+           # Holbrook,+MA
+def DistCalc(city):
+    _city = city
+                 # https://www.travelmath.com/driving-time/from/Lowell,+MA/to/Middleborough,+MA
+                 # <h3 class="space" id="drivetime">51 minutes</h3>
+    zvHttpTrip = r'https://www.travelmath.com/driving-time/from/Lowell,+MA/to/'+ _city
+            
+    zvTripOut = urllib.request.urlopen(zvHttpTrip).read()
+    zvTripOutFmt = zvTripOut.decode()
+    zvTxt = str(zvTripOutFmt)
+    zvTxt = zvTxt.split('<h3 class="space" id="drivetime">')[1]
+    zvTxt = zvTxt.split(' minutes</h3>')[0]
+    zvOut = zvTxt
 
-print(zvTripOutFmt)
+    return zvOut
+# ---------------------------------------------------------------------------------------------------
+
+
+# ---------------------------------------------------------------------------------------------------
+
+
+# Using readlines()
+file1 = open(InFile, 'r')
+Lines = file1.readlines()
+ 
+for line in Lines:
+    _ln = line.strip()
+
+    if _ln == 'Middleborough+Center,+MA':
+        print(_ln + '|1hr')
+
+    elif _ln == 'Lowell,+MA':
+        print(_ln + '|0')
+
+    elif _ln == 'Millis-Clicquot,+MA':
+        _ln = 'Millis,+MA'
+        print(_ln + '|' + DistCalc(_ln))
+
+    elif _ln == 'Mansfield+Center,+MA':
+        _ln = 'Mansfield,+MA'
+        print(_ln + '|' + DistCalc(_ln))
+
+    elif _ln == 'Norton+Center,+MA':
+        _ln = 'Norton,+MA'
+        print(_ln + '|' + DistCalc(_ln))
+
+    else:
+        print(_ln)
+        print(_ln + '|' + DistCalc(_ln))
+
+
+# print(DistCalc('Holbrook,+MA'))
